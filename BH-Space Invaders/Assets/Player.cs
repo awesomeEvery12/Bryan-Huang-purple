a43920f3bcdda91f2d764c88d878dd1dfc0aac0f
Hayden <leahmelanie999@gmail.com>
2023-05-07 16:01:18 -0400

    On step 35 May 7

--- BH-Space Invaders/Assets/Player.cs
+++ BH-Space Invaders/Assets/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public GameObject bulletPrefab;
     public float speed;
     // Update is called once per frame
     void Update()
@@ -17,5 +17,7 @@ public class Player : MonoBehaviour
         {
             transform.Translate(Vector2.left * Time.deltaTime * speed);
         }
+
+      
     }
 }
--- BH-Space Invaders/Assets/enemy2.cs
+++ BH-Space Invaders/Assets/enemy2.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class enemy : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    public float speed;
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector2.up * Time.deltaTime * speed);
         
     }
 }
