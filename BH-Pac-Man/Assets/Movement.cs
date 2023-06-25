af95f672306140b073afd4a97ad71bd0ca2b8fb0
Emma <126628086+Bamber22@users.noreply.github.com>
2023-06-25 15:59:15 -0400

    On step 81

---
+++ BH-Pac-Man/Assets/Movement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movement : MonoBehaviour
+{
+    public float speed;
+    public Vector2 initialDirection;
+    public LayerMask obstacleLayer;
+
+    protected Rigidbody2D rb;
+    protected Vector2 direction;
+    protected Vector2 nextDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initialDirection = Vector2.zero;
+        nextDirection = Vector2.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nextDirection != Vector2.zero)
+        {
+            SetDirection(nextDirection);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(position + translation);
+    }
+
+    private bool Occupied(Vector2 newDirection)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);
+        return hit.collider != null;
+    }
+    protected void SetDirection(Vector2 newDirection)
+    {
+        if(!Occupied(newDirection))
+        {
+            direction = newDirection;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection = newDirection;
+        }
+    }
+    abstract protected void ChildUpdate();
+}
